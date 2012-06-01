465d83ff61b0eeecf32bb1eb06d8dc38dc1fc6ff
Matthew Mirman <mmirman@palantir.com>
2012-06-01 15:52:26 -0700

    made mobius a comonad

--- Data/RegularManifold/TwoManifold/InfiniteMobius.hs
+++ Data/RegularManifold/TwoManifold/InfiniteMobius.hs
@@ -25,6 +25,19 @@ module Data.RegularManifold.TwoManifold.InfiniteMobius where
 import Control.UsefulCombinators
 import Control.Comonad
 import Data.Tuple (swap)
+{-
+
+import System.IO.Unsafe
+seqL l r = foldr seq r l
+moop = (\a -> seqL [a 1, a 2, a 4] ()) $ \i -> unsafePerformIO $ putStrLn $ "Lazy "++show i
+
+
+Idear:  Lazyness only evaluates it's argument once right?
+
+we can use unsafePerformIO with stateful things to make an affine type checker super compact
+-}
+
+getLeft (CNode m n) = CNode (left m) n
 
 -- | @'InfiniteMobius' a@ represents a viewpoint to a doubly linked 
 -- mobius data structure.  It also includes with it the 
@@ -39,14 +52,14 @@ data InfiniteMobiusN a = Node { left  :: InfiniteMobiusN a
                               , right :: InfiniteMobiusN a
                               }
 
-infMobiusN' :: Integer -> InfiniteMobius (Integer, Integer)
-infMobiusN' n = CNode outHead n  
+infMobiusN :: Integer -> InfiniteMobius (Integer, Integer)
+infMobiusN n = CNode outHead n  
   where (outHead, inHead) = makeStrip 0 (outHead, inHead)
         
         makeStrip v (pOutHead,pInHead) = (fOutHead, fInHead)
-          where (sOutHead,sInHead) = makeStrip (v+1) (fOutHead, fInHead)
-                (fOutHead,fOutTail) = makeWith v 0 fInTail sOutHead pInHead fInHead
-                (fInHead,fInTail)   = makeWith v n fOutTail pOutHead sInHead fOutHead
+          where ~(sOutHead,sInHead) = makeStrip (v+1) (fOutHead, fInHead)
+                ~(fOutHead,fOutTail) = makeWith v 0 fInTail sOutHead pInHead fInHead
+                ~(fInHead,fInTail)   = makeWith v n fOutTail pOutHead sInHead fOutHead
         
         makeWith v io = makeWith' (n - 1) where
           makeWith' c prev' up' down' end' = case c of
@@ -54,8 +67,33 @@ infMobiusN' n = CNode outHead n
               where tail' = Node prev' up' (v,io) down' end'
             _ -> (curr',tail')
               where curr' = Node prev' up' (v,c + io) down' next'
-                    (next',tail') = makeWith' (c-1) curr' (right up') (right down') end'
+                    ~(next',tail') = makeWith' (c-1) curr' (right up') (right down') end'
         
+instance Functor InfiniteMobius where
+  fmap = extend . (. extract)
+  
+instance Extend InfiniteMobius where
+  extend f (CNode outHeadOrg n) = CNode outHead n
+    where inHeadOrg = composeN left n outHeadOrg
+
+          (outHead, inHead) = makeStrip (outHeadOrg, inHeadOrg) (outHead, inHead)
+        
+          makeStrip (outOrg,inOrg) (pOutHead,pInHead) = (fOutHead, fInHead)
+            where ~(sOutHead,sInHead) = makeStrip (up outOrg, down inOrg) (fOutHead, fInHead)
+                  ~(fOutHead,fOutTail) = makeWith outOrg fInTail sOutHead pInHead fInHead
+                  ~(fInHead,fInTail)   = makeWith inOrg fOutTail pOutHead sInHead fOutHead
+        
+          makeWith v = makeWith' (n - 1) where
+            makeWith' c prev' up' down' end' = case c of
+              0 -> (tail', tail')
+                where tail' = Node prev' up' (f $ CNode v n) down' end'
+              _ -> (curr',tail')
+                where curr' = Node prev' up' (f $ CNode v n) down' next'
+                      ~(next',tail') = makeWith' (c-1) curr' (right up') (right down') end'
+
+instance Comonad InfiniteMobius where
+  extract = value . mobius
+  
 {-
 prod :: !!A -> B
 prod blah = (single :: A -> B) c 
